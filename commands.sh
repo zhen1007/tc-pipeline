586e9000a6157cd6366dd3f89ca1b2ff40b7e949
yahya-skillup <124680257+yahya-skillup@users.noreply.github.com>
2023-06-03 14:40:09 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -5,7 +5,7 @@ kubectl apply -f pipeline.yaml
 tkn pipeline start cd-pipeline \
     -p repo-url="https://github.com/yahya-skillup/tax_calculator.git" \
     -p branch=main \
-    -p app-name=hitcounter \
+    -p app-name=tax-calculator \
     -p build-image=image-registry.openshift-image-registry.svc:5000/$SN_ICR_NAMESPACE/tekton-lab:latest \
     -w name=pipeline-workspace,claimName=pipelinerun-pvc \
     --showlog
