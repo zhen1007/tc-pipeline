6b1f90a9209a79b4c424db01a73a9ab081740dbc
yahya-skillup <124680257+yahya-skillup@users.noreply.github.com>
2023-06-04 17:06:49 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,8 +1,19 @@
 tkn hub install task git-clone
 
+kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
+
 kubectl apply -f tasks.yaml
 kubectl apply -f pvc.yaml
 kubectl apply -f pipeline.yaml
+
+tkn pipeline start cd-pipeline \
+    -p repo-url="https://github.com/yahya-skillup/tax_calculator.git" \
+    -p branch=main \
+    -p build-image=image-registry.openshift-image-registry.svc:5000/$SN_ICR_NAMESPACE/tekton-lab:latest \
+    -w name=pipeline-workspace,claimName=pipelinerun-pvc \
+    --showlog
+
+tkn pipelinerun ls
 
 tkn pipeline start cd-pipeline \
     -p repo-url="https://github.com/yahya-skillup/tax_calculator.git" \
