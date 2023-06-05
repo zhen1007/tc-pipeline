246c75f14b2fbe5ca6c1f304f94022baf353781b
yahya-skillup <124680257+yahya-skillup@users.noreply.github.com>
2023-06-05 14:15:55 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -22,3 +22,11 @@ tkn pipelinerun ls
 tkn pipelinerun logs --last
 
 kubectl get all -l app=tax-calculator
+
+
+
+IBMCLOUD_API_KEY=THE_KEY
+ibmcloud login --apikey $IBMCLOUD_API_KEY
+ibmcloud target -g production
+ibmcloud ce project select --id d8db2fbf-REMAINING_ID
+ibmcloud ce project current
