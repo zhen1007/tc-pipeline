303b57a84901415e1a8c45121f0ea341b6bfebf2
yahya-skillup <124680257+yahya-skillup@users.noreply.github.com>
2023-06-06 22:54:11 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,9 +1,14 @@
 tkn hub install task git-clone
 tkn hub install task docker-build
+tkn hub install task ibmcloud
+
+tkn task start ibmcloud 
+
 
 tkn clustertask ls
 
 kubectl apply -f https://raw.githubusercontent.com/tektoncd/catalog/main/task/git-clone/0.9/git-clone.yaml
+kubectl apply -f https://api.hub.tekton.dev/v1/resource/tekton/task/ibmcloud/0.1/raw
 
 kubectl apply -f tasks.yaml
 kubectl apply -f pvc.yaml
@@ -31,3 +36,15 @@ ibmcloud target -g production
 ibmcloud ce project current | grep ID | awk '{split($2, a, ":"); print a[1]}'
 ibmcloud ce project select --id d8db2fbf-REMAINING_ID
 ibmcloud ce project current
+
+
+
+tkn pipeline start cd-pipeline \
+    -p repo-url="https://github.com/yahya-skillup/tax_calculator.git" \
+    -p branch=main \
+    -p app-name=tax-calculator \
+    -p build-image=image-registry.openshift-image-registry.svc:5000/$SN_ICR_NAMESPACE/tax-calculator \
+    -p ibmcloud-api-key= $IBMCLOUD_API_KEY \
+    -p ibmcloud-project-id= 199ccf6d-79b9-4d60-95f7-e26052736f50 \
+    -w name=pipeline-workspace,claimName=pipelinerun-pvc \
+    --showlog
